bf82c633dd3246b0863e7d83fa6eb24a35ac1e39
henry <nguyentuanhoang1994@yahoo.com>
2023-03-17 01:56:59 -0700

    init

---
+++ todo-app/src/Todo.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const Todo = ( {
+    task = "default todo",
+    id = "1",
+    remove,
+    update } ) =>
+{
+    const [ editTask, setEditTask ] = useState( task );
+    const [ isEditing, setIsEditing ] = useState( false );
+
+    const toggleEdit = () =>
+    {
+        setIsEditing( edit => !edit );
+    };
+
+    const handleChange = e =>
+    {
+        setEditTask( e.target.value );
+    };
+
+    const handleDelete = () => remove( id );
+
+    const handleUpdate = e =>
+    {
+        e.preventDefault();
+        update( id, editTask );
+        setIsEditing( false );
+    };
+
+    // default todo view
+    let jsx = (
+        <div>
+            <li>{task}</li>
+            <button onClick={toggleEdit}>Edit</button>
+            <button onClick={handleDelete}>X</button>
+        </div>
+    );
+
+    //todo view when editing
+    if ( isEditing )
+    {
+        jsx = (
+            <div>
+                <form onSubmit={handleUpdate}>
+                    <input
+                        type="test"
+                        value={editTask}
+                        onChange={handleChange}
+                    />
+                    <button>Update!</button>
+                </form>
+            </div>
+        )
+    }
+
+    return jsx;
+}
+
+export default Todo;
